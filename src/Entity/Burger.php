cf4bc5c876745dbcd5b7bced00d76856bd53c3ff
tijou allan <tijouallan@gmail.com>
2023-11-16 09:49:17 +0100

    creation bdd

---
+++ src/Entity/Burger.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BurgerRepository::class)]
+class Burger
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prix = null;
+}
---
+++ src/Entity/Sauce.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SauceRepository::class)]
+class Sauce
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+}
